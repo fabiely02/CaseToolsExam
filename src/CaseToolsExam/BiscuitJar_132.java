43e6d9b2d24246afcdd5597aa62721c7119d030a
exam <exam@Bcalab4-10>
2023-05-03 12:24:27 +0530

    second commit

--- src/CaseToolsExam/BiscuitJar_132.java
+++ src/CaseToolsExam/BiscuitJar_132.java
@@ -1,7 +1,16 @@
 package CaseToolsExam;
 
 public class BiscuitJar_132 {
+	int num_biscuits;
+	BiscuitJar_132(int num_biscuits)
+	{
+		this.num_biscuits=num_biscuits;
+	}
 
+	int get_biscuit_count()
+	{
+		return num_biscuits;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
